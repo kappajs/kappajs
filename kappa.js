d2e45faa59fc86a35d515290e363d5bb1e9fb736
Joseph Campuzano <campuzanojoseph@gmail.com>
2018-08-10 17:03:46 -0500

    small refactor

--- kappa.js
+++ kappa.js
@@ -1,37 +1,48 @@
+import { render } from "./node_modules/lit-html";
+
 let componentMap = {};
 
 export default class Kappa {
-  
   constructor(el) {
     this.el = el;
-    console.log('Kappa bootstrapped');
+    console.log("Kappa bootstrapped");
   }
 }
 
 export function component(name, template, definition) {
-  if (componentMap[name]) throw new Error(`The component ${name} is already registered`);
-  const component = new KappaComponent(definition, template);
-  window.customElements.define(name, component);
-  return component;
+  if (componentMap[name])
+    throw new Error(`The component ${name} is already registered`);
+
+  const webComponent = createKappaComponent(template, definition);
+  componentMap[name] = webComponent;
+  window.customElements.define(name, webComponent);
 }
 
-export class KappaComponent extends HTMLElement {
-  constructor(definition, template) {
-    super();
-    this.definition = definition;
-    const shadow = this.attachShadow({ mode: 'open'});
-    shadow.innerHTML = template;
-  }
+export function createKappaComponent(template, definition) {
+  return class extends HTMLElement {
+    constructor() {
+      super();
 
-  connectedCallback() {
-    definition.created();
-  }
+      this.definition = definition;
+      const shadow = this.attachShadow({ mode: "open" });
+      const container = document.createDocumentFragment();
+      render(this._render(), container);
+      shadow.appendChild(container);
+      shadow.innerHTML = template;
+    }
 
-  disconnectedCallback() {
-    
-  }
+    _render() {
+      return template;
+    }
 
-  attributeChangedCallback(attrName, oldVal, newVal) {
-    definition.updated();
-  }
+    connectedCallback() {
+      definition.created();
+    }
+
+    disconnectedCallback() {}
+
+    attributeChangedCallback(attrName, oldVal, newVal) {
+      definition.updated();
+    }
+  };
 }
